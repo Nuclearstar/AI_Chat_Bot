e9dc78be34f40df9b9e3b82bb0fbab00f57e3fd6
Nithin Prabhu <nithingprabhu@gmail.com>
2017-07-30 10:50:53 +0530

    Add files via upload

---
+++ Exmp.py
@@ -0,0 +1,36 @@
+import chatterbot
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+#C:\Users\User\AppData\Local\Programs\Python\Python36-32\Lib\site-packages\chatterbot_corpus\data\english
+chatbot = ChatBot('Ron Obvious',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    input_adapter='chatterbot.input.TerminalAdapter',
+    output_adapter='chatterbot.output.TerminalAdapter',
+    trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
+    logic_adapters = ['chatterbot.logic.MathematicalEvaluation',
+                      
+                     {
+                    'import_path': 'chatterbot.logic.SpecificResponseAdapter',
+                    'input_text': 'Help me!',
+                    'output_text': 'Ok, here is a link: http://google.com'
+                    },
+                    {
+                    'import_path': 'chatterbot.logic.BestMatch'
+                    },
+                    {
+                    'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+                    'threshold': 0.5,
+                    'default_response': 'I am sorry, but I do not understand.'
+                    }],
+       database='./database.sqlite3')
+
+print('Type something to begin...')
+# Train based on the english corpus
+chatbot.train("chatterbot.corpus.english")
+
+while True:
+    try:
+     bot_input = chatbot.get_response(chatterbot.logic.MultiLogicAdapter)
+
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
