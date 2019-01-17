c7d28dea016312ff8480050c42b4af04d9272608
Nithin Prabhu <nithingprabhu@gmail.com>
2019-01-17 15:35:26 +0530

    Update Exmp.py

--- Exmp.py
+++ Exmp.py
@@ -4,6 +4,7 @@ I took the help of chatterbot Python API and the various modules
 used here are not provided in this repository at present.
 This is just a part taken from the code.
 """
+import telebot
 import chatterbot
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
@@ -29,15 +30,23 @@ chatbot = ChatBot('Ron Obvious',
                     }],
        database='./database.sqlite3')
 
-print('Type something to begin...')
-
 # Train based on the english corpus
 chatbot.train("chatterbot.corpus.english")
 
-# The following loop will execute each time the user enters input
-while True:
-    try:
-     bot_input = chatbot.get_response(chatterbot.logic.MultiLogicAdapter)
+API_TOKEN = ' ' #Enter your telegram bot token within quotes
 
-    except(KeyboardInterrupt, EOFError, SystemExit):
-        break
+bot = telebot.TeleBot(API_TOKEN)
+
+# Handle '/start'
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Okay Let's start chatting")
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot_input = message.text+""
+    bot_output = chatbot.get_response(bot_input)
+    bot.reply_to(message, bot_output)
+
+bot.polling()
